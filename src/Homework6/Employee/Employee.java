0dc29f5b63d66cb193e928249b2ff910f0fe8d1c
irinagorlach <goirinagorlach@yandex.by>
2024-05-27 15:35:53 +0300

    add Main.java Park.java Employee.java

---
+++ src/Homework6/Employee/Employee.java
@@ -0,0 +1,30 @@
+package Homework6.Employee;
+
+public class Employee {
+    String fullName;
+    String position;
+    String email;
+    String phoneNumber;
+    int salary;
+    int age;
+
+    public Employee(String fullName, String position, String email, String phoneNumber, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void printObjectInformation() {
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Должность: " + position);
+        System.out.println("Email: " + email);
+        System.out.println("Телефон: " + phoneNumber);
+        System.out.println("Зарплата: " + salary);
+        System.out.println("Возраст: " + age);
+    }
+}
+
+
---
+++ src/Homework6/Park/Park.java
@@ -0,0 +1,48 @@
+package Homework6.Park;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Park {
+
+    public class Attraction {
+        private String attractionName;
+        private String attractionWorkingHours;
+        private int attractionPrice;
+
+        public Attraction(String attractionName, String attractionWorkingHours, int attractionPrice) {
+            this.attractionName = attractionName;
+            this.attractionWorkingHours = attractionWorkingHours;
+            this.attractionPrice = attractionPrice;
+        }
+    }
+
+    private List<Attraction> attractions;
+    private int maxNumberOfAttractions;
+
+    public Park(int maxNumberOfAttractions) {
+        this.attractions = new ArrayList<>();
+        this.maxNumberOfAttractions = maxNumberOfAttractions;
+    }
+
+    public void addAttraction(String attractionName, String attractionWorkingHours, int attractionPrice) {
+        if (attractions.size() < maxNumberOfAttractions) {
+            Attraction attraction = new Attraction(attractionName, attractionWorkingHours, attractionPrice);
+            attractions.add(attraction);
+        } else {
+            System.out.println("Невозможно выполнить добавление аттракциона. В парке нет места.");
+        }
+    }
+
+    public void printAttractionsInfo() {
+        for (int i = 0; i < attractions.size(); i++) {
+            System.out.println("--------------------");
+            Attraction attraction = attractions.get(i);
+            System.out.println("Название аттракциона: " + attraction.attractionName);
+            System.out.println("Время работы аттракциона: " + attraction.attractionWorkingHours);
+            System.out.println("Стоимость аттракциона: " + attraction.attractionPrice);
+        }
+    }
+
+}
