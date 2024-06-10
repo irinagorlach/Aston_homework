607209249db47e260de3ed9238d71bf8b8d680cb
irinagorlach <goirinagorlach@yandex.by>
2024-06-10 15:48:50 +0300

    add Animal.java Animal/Main.java Shapes.java Shapes/Main.java

---
+++ src/Homework10/Animal/Main.java
@@ -0,0 +1,34 @@
+package Homework10.Animal;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Animal animal = new Animal("Лев");
+        Dog dog = new Dog("пес Дружок");
+        Cat cat1 = new Cat("кот Тигрик");
+
+        dog.run(500);
+        cat1.run(200);
+        dog.swim(10);
+        cat1.swim(10);
+
+        Bowl bowl = new Bowl(20);
+        Cat[] cats = {new Cat("Плохиш"), new Cat("Гаражик"), new Cat("Попрыгун"), new Cat("Джеки")};
+
+        for (Cat cat : cats) {
+            cat.eatFromBowl(bowl);
+        }
+
+        for (Cat cat : cats) {
+            System.out.println(cat.getName() + " сыт: " + cat.full());
+        }
+
+        bowl.addFood(10);
+
+        System.out.println("-----------------");
+        System.out.println("Всего животных: " + Animal.getCountAnimal());
+        System.out.println("Всего собак: " + Dog.getCountDog());
+        System.out.println("Всего котов: " + Cat.getCountCat());
+    }
+
+}
