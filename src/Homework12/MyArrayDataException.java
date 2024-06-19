674aa5dcc3119be997b0a46b642f20cce48c5b1c
irinagorlach <goirinagorlach@yandex.by>
2024-06-19 08:57:05 +0300

    add Homework13

--- src/Homework12/MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package Homework12;
-
-public class MyArrayDataException extends Exception {
-    public MyArrayDataException(String message) {
-        super(message);
-    }
-}
--- src/Homework12/MyArraySizeException.java
+++
@@ -1,7 +0,0 @@
-package Homework12;
-
-public class MyArraySizeException extends Exception {
-    public MyArraySizeException(String message) {
-        super(message);
-    }
-}
