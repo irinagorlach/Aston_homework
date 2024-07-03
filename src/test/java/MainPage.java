fb96e1ea9eb698ce08b4c6ed48951eb9d4ae4f81
irinagorlach <goirinagorlach@yandex.by>
2024-07-03 20:24:03 +0300

    add Homework16

--- src/test/java/MainPage.java
+++ src/test/java/MainPage.java
@@ -1,7 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -33,54 +32,19 @@ public class MainPage {
 
     @Test
     @Order(1)
-    public void testBlockTitle() {
-        WebElement blockTitle = driver.findElement(By.xpath("//h2[contains(text(), 'Онлайн пополнение')]"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", blockTitle);
+    public void testCheckInscriptionsInTheBlock() {
+        WebElement inscriptions = driver.findElement(By.className("select__wrapper"));
+        inscriptions.click();
 
-        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(By.className("pay")));
-        Assert.assertEquals(blockTitle.getText(), "Онлайн пополнение\n" + "без комиссии");
+        WebElement selectList = driver.findElement(By.className("select__list"));
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(By.className("select__list")));
+        Assert.assertEquals(selectList.getText(), "Услуги связи\n" + "Домашний интернет\n" + "Рассрочка\n" + "Задолженность");
     }
 
     @Test
     @Order(2)
-    public void testPaymentSystemLogos() {
-        WebElement visaIcon = driver.findElement(By.xpath("//div[@class='pay__partners']//img[@src='/local/templates/new_design/assets/html/images/pages/index/pay/visa.svg']"));
-        boolean visaIconPresent = visaIcon.isDisplayed();
-        Assert.assertTrue(visaIconPresent);
-
-        WebElement visaVerifiedIcon = driver.findElement(By.xpath("//div[@class='pay__partners']//img[@src='/local/templates/new_design/assets/html/images/pages/index/pay/visa-verified.svg']"));
-        boolean visaVerifiedIconPresent = visaVerifiedIcon.isDisplayed();
-        Assert.assertTrue(visaVerifiedIconPresent);
-
-        WebElement mastercardIcon = driver.findElement(By.xpath("//div[@class='pay__partners']//img[@src='/local/templates/new_design/assets/html/images/pages/index/pay/mastercard.svg']"));
-        boolean mastercardIconPresent = mastercardIcon.isDisplayed();
-        Assert.assertTrue(mastercardIconPresent);
-
-        WebElement mastercardSecureIcon = driver.findElement(By.xpath("//div[@class='pay__partners']//img[@src='/local/templates/new_design/assets/html/images/pages/index/pay/mastercard-secure.svg']"));
-        boolean mastercardSecureIconPresent = mastercardSecureIcon.isDisplayed();
-        Assert.assertTrue(mastercardSecureIconPresent);
-
-        WebElement belkartIcon = driver.findElement(By.xpath("//div[@class='pay__partners']//img[@src='/local/templates/new_design/assets/html/images/pages/index/pay/belkart.svg']"));
-        boolean belkartIconPresent = belkartIcon.isDisplayed();
-        Assert.assertTrue(belkartIconPresent);
-    }
-
-    @Test
-    @Order(3)
-    public void testMoreAboutServiceLink() {
-        WebElement moreAboutServiceLink = driver.findElement(By.linkText("Подробнее о сервисе"));
-        moreAboutServiceLink.click();
-
-        new WebDriverWait(driver, 5).until(ExpectedConditions.urlToBe("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/"));
-        String newUrl = driver.getCurrentUrl();
-        Assert.assertEquals("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/", newUrl);
-    }
-
-    @Test
-    @Order(4)
-    public void testContinueButton() {
+    public void testCheckDisplayPrice() {
         driver.get("https://www.mts.by/");
-
         WebElement phoneInputField = driver.findElement(By.id("connection-phone"));
         phoneInputField.click();
         phoneInputField.sendKeys("297777777");
@@ -100,10 +64,237 @@ public class MainPage {
         new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(By.className("bepaid-iframe")));
         driver.switchTo().frame(driver.findElement(By.className("bepaid-iframe")));
 
-        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(By.id("cc-number")));
-        WebElement textSum = driver.findElement(By.xpath("//span[contains(text(), '30.00 BYN')]"));
+        new WebDriverWait(driver, 5).until(ExpectedConditions.textToBe(By.xpath("//span[contains(text(), '30.00 BYN')]"), "30.00 BYN"));
+        WebElement textPrice = driver.findElement(By.xpath("//span[contains(text(), '30.00 BYN')]"));
 
         new WebDriverWait(driver, 10).until(ExpectedConditions.textToBe(By.xpath("//span[contains(text(), '30.00 BYN')]"), "30.00 BYN"));
-        Assert.assertEquals(textSum.getText(), "30.00 BYN");
+        Assert.assertEquals(textPrice.getText(), "30.00 BYN");
+    }
+
+    @Test
+    @Order(3)
+    public void testCheckDisplayPriceOnButton() {
+        driver.get("https://www.mts.by/");
+        WebElement phoneInputField = driver.findElement(By.id("connection-phone"));
+        phoneInputField.click();
+        phoneInputField.sendKeys("297777777");
+
+        WebElement sumInputField = driver.findElement(By.id("connection-sum"));
+        sumInputField.click();
+        sumInputField.sendKeys("30");
+
+        WebElement emailField = driver.findElement(By.id("connection-email"));
+        emailField.click();
+        emailField.sendKeys("345nameName@gmail.com");
+
+        WebElement continueButton = driver.findElement(By.cssSelector("#pay-connection .button"));
+        new WebDriverWait(driver, 5).until(ExpectedConditions.elementToBeClickable(By.cssSelector("#pay-connection .button")));
+        continueButton.click();
+
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(By.className("bepaid-iframe")));
+        driver.switchTo().frame(driver.findElement(By.className("bepaid-iframe")));
+
+        new WebDriverWait(driver, 10).until(ExpectedConditions.textToBe(By.xpath("//button[@type='submit']"), "Оплатить 30.00 BYN"));
+        WebElement textPriceButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        Assert.assertEquals(textPriceButton.getText(), "Оплатить 30.00 BYN");
+    }
+
+    @Test
+    @Order(4)
+    public void testCheckDisplayPhone() {
+        driver.get("https://www.mts.by/");
+        WebElement phoneInputField = driver.findElement(By.id("connection-phone"));
+        phoneInputField.click();
+        phoneInputField.sendKeys("297777777");
+
+        WebElement sumInputField = driver.findElement(By.id("connection-sum"));
+        sumInputField.click();
+        sumInputField.sendKeys("30");
+
+        WebElement emailField = driver.findElement(By.id("connection-email"));
+        emailField.click();
+        emailField.sendKeys("345nameName@gmail.com");
+
+        WebElement continueButton = driver.findElement(By.cssSelector("#pay-connection .button"));
+        new WebDriverWait(driver, 5).until(ExpectedConditions.elementToBeClickable(By.cssSelector("#pay-connection .button")));
+        continueButton.click();
+
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(By.className("bepaid-iframe")));
+        driver.switchTo().frame(driver.findElement(By.className("bepaid-iframe")));
+
+        new WebDriverWait(driver, 5).until(ExpectedConditions.textToBe(By.xpath("//span[contains(text(), '30.00 BYN')]"), "30.00 BYN"));
+        WebElement textPhoneNumber = driver.findElement(By.className("pay-description__text"));
+
+        new WebDriverWait(driver, 10).until(ExpectedConditions.textToBe(By.xpath("//span[contains(text(), 'Номер:375297777777')]"), "Оплата: Услуги связи Номер:375297777777"));
+        Assert.assertEquals(textPhoneNumber.getText(), "Оплата: Услуги связи Номер:375297777777");
+    }
+
+    @Test
+    @Order(5)
+    public void testCheckСardNumber() {
+        driver.get("https://www.mts.by/");
+        WebElement phoneInputField = driver.findElement(By.id("connection-phone"));
+        phoneInputField.click();
+        phoneInputField.sendKeys("297777777");
+
+        WebElement sumInputField = driver.findElement(By.id("connection-sum"));
+        sumInputField.click();
+        sumInputField.sendKeys("30");
+
+        WebElement emailField = driver.findElement(By.id("connection-email"));
+        emailField.click();
+        emailField.sendKeys("345nameName@gmail.com");
+
+        WebElement continueButton = driver.findElement(By.cssSelector("#pay-connection .button"));
+        new WebDriverWait(driver, 5).until(ExpectedConditions.elementToBeClickable(By.cssSelector("#pay-connection .button")));
+        continueButton.click();
+
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(By.className("bepaid-iframe")));
+        driver.switchTo().frame(driver.findElement(By.className("bepaid-iframe")));
+
+        new WebDriverWait(driver, 5).until(ExpectedConditions.textToBe(By.xpath("//span[contains(text(), '30.00 BYN')]"), "30.00 BYN"));
+        WebElement textCardNumber = driver.findElement(By.xpath("//*[contains(text(), 'Номер карты')]"));
+
+        new WebDriverWait(driver, 10).until(ExpectedConditions.textToBe(By.xpath("//*[contains(text(), 'Номер карты')]"), "Номер карты"));
+        Assert.assertEquals(textCardNumber.getText(), "Номер карты");
+    }
+
+    @Test
+    @Order(6)
+    public void testCheckСardValidity() {
+        driver.get("https://www.mts.by/");
+        WebElement phoneInputField = driver.findElement(By.id("connection-phone"));
+        phoneInputField.click();
+        phoneInputField.sendKeys("297777777");
+
+        WebElement sumInputField = driver.findElement(By.id("connection-sum"));
+        sumInputField.click();
+        sumInputField.sendKeys("30");
+
+        WebElement emailField = driver.findElement(By.id("connection-email"));
+        emailField.click();
+        emailField.sendKeys("345nameName@gmail.com");
+
+        WebElement continueButton = driver.findElement(By.cssSelector("#pay-connection .button"));
+        new WebDriverWait(driver, 5).until(ExpectedConditions.elementToBeClickable(By.cssSelector("#pay-connection .button")));
+        continueButton.click();
+
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(By.className("bepaid-iframe")));
+        driver.switchTo().frame(driver.findElement(By.className("bepaid-iframe")));
+
+        new WebDriverWait(driver, 5).until(ExpectedConditions.textToBe(By.xpath("//span[contains(text(), '30.00 BYN')]"), "30.00 BYN"));
+        WebElement textCardNumber = driver.findElement(By.xpath("//*[contains(text(), 'Срок действия')]"));
+
+        new WebDriverWait(driver, 10).until(ExpectedConditions.textToBe(By.xpath("//*[contains(text(), 'Срок действия')]"), "Срок действия"));
+        Assert.assertEquals(textCardNumber.getText(), "Срок действия");
+    }
+
+    @Test
+    @Order(7)
+    public void testCheckСardCVC() {
+        driver.get("https://www.mts.by/");
+        WebElement phoneInputField = driver.findElement(By.id("connection-phone"));
+        phoneInputField.click();
+        phoneInputField.sendKeys("297777777");
+
+        WebElement sumInputField = driver.findElement(By.id("connection-sum"));
+        sumInputField.click();
+        sumInputField.sendKeys("30");
+
+        WebElement emailField = driver.findElement(By.id("connection-email"));
+        emailField.click();
+        emailField.sendKeys("345nameName@gmail.com");
+
+        WebElement continueButton = driver.findElement(By.cssSelector("#pay-connection .button"));
+        new WebDriverWait(driver, 5).until(ExpectedConditions.elementToBeClickable(By.cssSelector("#pay-connection .button")));
+        continueButton.click();
+
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(By.className("bepaid-iframe")));
+        driver.switchTo().frame(driver.findElement(By.className("bepaid-iframe")));
+
+        new WebDriverWait(driver, 5).until(ExpectedConditions.textToBe(By.xpath("//span[contains(text(), '30.00 BYN')]"), "30.00 BYN"));
+        WebElement textCardNumber = driver.findElement(By.xpath("//*[contains(text(), 'CVC')]"));
+
+        new WebDriverWait(driver, 10).until(ExpectedConditions.textToBe(By.xpath("//*[contains(text(), 'CVC')]"), "CVC"));
+        Assert.assertEquals(textCardNumber.getText(), "CVC");
+    }
+
+    @Test
+    @Order(8)
+    public void testCheckСardNameHolder() {
+        driver.get("https://www.mts.by/");
+        WebElement phoneInputField = driver.findElement(By.id("connection-phone"));
+        phoneInputField.click();
+        phoneInputField.sendKeys("297777777");
+
+        WebElement sumInputField = driver.findElement(By.id("connection-sum"));
+        sumInputField.click();
+        sumInputField.sendKeys("30");
+
+        WebElement emailField = driver.findElement(By.id("connection-email"));
+        emailField.click();
+        emailField.sendKeys("345nameName@gmail.com");
+
+        WebElement continueButton = driver.findElement(By.cssSelector("#pay-connection .button"));
+        new WebDriverWait(driver, 5).until(ExpectedConditions.elementToBeClickable(By.cssSelector("#pay-connection .button")));
+        continueButton.click();
+
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(By.className("bepaid-iframe")));
+        driver.switchTo().frame(driver.findElement(By.className("bepaid-iframe")));
+
+        new WebDriverWait(driver, 5).until(ExpectedConditions.textToBe(By.xpath("//span[contains(text(), '30.00 BYN')]"), "30.00 BYN"));
+        WebElement textCardNumber = driver.findElement(By.xpath("//*[contains(text(), 'Имя держателя (как на карте)')]"));
+
+        new WebDriverWait(driver, 10).until(ExpectedConditions.textToBe(By.xpath("//*[contains(text(), 'Имя держателя (как на карте)')]"), "Имя держателя (как на карте)"));
+        Assert.assertEquals(textCardNumber.getText(), "Имя держателя (как на карте)");
+    }
+
+    @Test
+    @Order(9)
+    public void testPaymentSystemLogos() {
+        driver.get("https://www.mts.by/");
+        WebElement phoneInputField = driver.findElement(By.id("connection-phone"));
+        phoneInputField.click();
+        phoneInputField.sendKeys("297777777");
+
+        WebElement sumInputField = driver.findElement(By.id("connection-sum"));
+        sumInputField.click();
+        sumInputField.sendKeys("30");
+
+        WebElement emailField = driver.findElement(By.id("connection-email"));
+        emailField.click();
+        emailField.sendKeys("345nameName@gmail.com");
+
+        WebElement continueButton = driver.findElement(By.cssSelector("#pay-connection .button"));
+        new WebDriverWait(driver, 5).until(ExpectedConditions.elementToBeClickable(By.cssSelector("#pay-connection .button")));
+        continueButton.click();
+
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(By.className("bepaid-iframe")));
+        driver.switchTo().frame(driver.findElement(By.className("bepaid-iframe")));
+
+        new WebDriverWait(driver, 10).until(ExpectedConditions.textToBe(By.xpath("//span[contains(text(), '30.00 BYN')]"), "30.00 BYN"));
+
+        WebElement mastercardIcon = driver.findElement(By.xpath("//div[contains(@class, 'cards-brands__container')]/img[@src='assets/images/payment-icons/card-types/mastercard-system.svg']"));
+        boolean mastercardIconPresent = mastercardIcon.isDisplayed();
+        Assert.assertTrue(mastercardIconPresent);
+
+        WebElement visaIcon = driver.findElement(By.xpath("//div[contains(@class, 'cards-brands__container')]/img[@src='assets/images/payment-icons/card-types/visa-system.svg']"));
+        boolean visaIconPresent = visaIcon.isDisplayed();
+        Assert.assertTrue(visaIconPresent);
+
+        WebElement belkartIcon = driver.findElement(By.xpath("//div[contains(@class, 'cards-brands__container')]/img[@src='assets/images/payment-icons/card-types/belkart-system.svg']"));
+        boolean belkartIconPresent = belkartIcon.isDisplayed();
+        Assert.assertTrue(belkartIconPresent);
+
+        WebElement mirIcon = driver.findElement(By.xpath("//div[contains(@class, 'cards-brands__container')]/img[@src='assets/images/payment-icons/card-types/belkart-system.svg']"));
+        boolean mirIconPresent = mirIcon.isDisplayed();
+        Assert.assertTrue(mirIconPresent);
+
+        new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'cards-brands_random')]/img[@src='assets/images/payment-icons/card-types/maestro-system.svg']")));
+        WebElement maestroIcon = driver.findElement(By.xpath("//div[contains(@class, 'cards-brands_random')]/img[@src='assets/images/payment-icons/card-types/maestro-system.svg']"));
+        boolean maestroIconPresent = maestroIcon.isDisplayed();
+        Assert.assertTrue(maestroIconPresent);
     }
 }
+
+
